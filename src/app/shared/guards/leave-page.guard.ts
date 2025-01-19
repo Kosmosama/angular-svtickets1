039cb4a2076496f2329cbdd1ac8a4a4f88a51139
Kosmosama <luismg0408@gmail.com>
2025-01-19 21:03:49 +0100

    Refactor: Menu changes.

--- src/app/shared/guards/leave-page.guard.ts
+++ src/app/shared/guards/leave-page.guard.ts
@@ -9,3 +9,4 @@ export const leavePageGuard: CanDeactivateFn<CanComponentDeactivate> = (componen
 // '/auth/register' routes. Only ask the user if the he/she has changed something
 // in the form (dirty), or if the data has not been saved yet. Use ngBootstrap
 // modal to ask the user.
+// (I think it is done)
